224f8ce1f2dcca57de1f5a7fa9a2815d7676d889
Michael Stev Cardenas Quintero <sniperwolf2313@gmail.com>
2026-03-03 16:42:36 -0500

    Actualización terraform

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,15 @@
-output "versions_bucket" { value = aws_s3_bucket.eb_versions.bucket }
-output "app_name"        { value = aws_elastic_beanstalk_application.app.name }
-output "env_name"        { value = aws_elastic_beanstalk_environment.env.name }
-output "env_url"         { value = aws_elastic_beanstalk_environment.env.endpoint_url }
+output "versions_bucket" {
+  value = aws_s3_bucket.eb_versions.bucket
+}
+
+output "app_name" {
+  value = aws_elastic_beanstalk_application.app.name
+}
+
+output "env_name" {
+  value = aws_elastic_beanstalk_environment.env.name
+}
+
+output "env_url" {
+  value = aws_elastic_beanstalk_environment.env.endpoint_url
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,18 @@
-variable "project"    { type = string }              # prefijo para nombrar recursos
-variable "aws_region" {
+variable "project" {
   type = string
+}
+
+variable "aws_region" {
+  type    = string
   default = "us-east-1"
 }
 
-variable "app_name"   {
-  type = string
+variable "app_name" {
+  type    = string
   default = "demo-backend-app"
 }
-variable "env_name"   {
-  type = string
+
+variable "env_name" {
+  type    = string
   default = "demo-backend-env"
 }
