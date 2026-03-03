bd8935223d2bd7c00445965255560ff50c555f36
Michael Stev Cardenas Quintero <sniperwolf2313@gmail.com>
2026-03-03 17:25:37 -0500

    Actualización terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket  = "demo-backend-tfstate-317196639027"
+    key     = "demo-backend/infra/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
   required_providers {
     aws    = { source = "hashicorp/aws", version = "~> 5.0" }
     random = { source = "hashicorp/random", version = "~> 3.0" }
